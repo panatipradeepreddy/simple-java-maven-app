70cd62cfcdfb22c10241e252505fc7d5ce35d495
panatipradeepreddy <59985709+panatipradeepreddy@users.noreply.github.com>
2020-02-04 10:54:51 +0530

    updated source code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Today is 4th Feb 2020";
+    private final String message = "Hello World! Today is 4th Feb 2020!";
 
     public App() {}
 
